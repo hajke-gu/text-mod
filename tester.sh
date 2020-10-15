1f57d9534853555ec557cd42254d8541063cef73
Synoecism <karl.westgardh@gmail.com>
2020-10-15 11:32:33 +0200

    simply tester and change yml file

--- tester.sh
+++ tester.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install gnome-terminal
 npm i
-USERNAME=$1
-PASSWORD=$2
-gnome-terminal -?
-gnome-terminal --display=0
-gnome-terminal --display=:0.0
-gnome-terminal -- --display=0 /bin/sh -c 'node puppeteer.js -u $USERNAME -p $PASSWORD -h' && npm run start
+echo "hello sir from the tester"
