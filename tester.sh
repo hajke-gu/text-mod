aeb58c713bd68a858e33db053933dbc23e8e5358
Synoecism <karl.westgardh@gmail.com>
2020-10-15 11:22:46 +0200

    test another gnome-terminal option

--- tester.sh
+++ tester.sh
@@ -6,5 +6,6 @@ npm i
 USERNAME=$1
 PASSWORD=$2
 gnome-terminal -?
-gnome-termina --display=0
+gnome-terminal --display=0
+gnome-terminal --display=:0.0
 gnome-terminal -- --display=0 /bin/sh -c 'node puppeteer.js -u $USERNAME -p $PASSWORD -h' && npm run start
