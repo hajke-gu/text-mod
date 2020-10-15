f8062bc397f863d64a512eb38626ece3525c8801
Synoecism <karl.westgardh@gmail.com>
2020-10-15 11:19:12 +0200

    test another display option

--- tester.sh
+++ tester.sh
@@ -5,6 +5,6 @@ sudo apt-get install gnome-terminal
 npm i
 USERNAME=$1
 PASSWORD=$2
-export DISPLAY=:0.0 #required since there is no display
 gnome-terminal -?
-gnome-terminal -- /bin/sh -c 'node puppeteer.js -u $USERNAME -p $PASSWORD -h' && npm run start
+gnome-termina --display=0
+gnome-terminal -- --display=0 /bin/sh -c 'node puppeteer.js -u $USERNAME -p $PASSWORD -h' && npm run start
