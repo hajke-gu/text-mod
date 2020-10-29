b466263d7d2bf277663280a64cd9d99a9dea2913
karl westgårdh <karl.westgardh@gmail.com>
2020-10-29 11:28:35 +0100

    refactor of test js

--- puppeteer.js
+++ puppeteer.js
@@ -1,8 +1,9 @@
 const puppeteer = require("puppeteer");
 var argv = require("minimist")(process.argv.slice(2));
 
+/* main runner */
 (async () => {
-    /* setup */
+    /* start */
     var headless;
 
     if (argv.h) {
@@ -11,8 +12,9 @@ var argv = require("minimist")(process.argv.slice(2));
     } else {
         headless = false;
     }
-    const browser = await puppeteer.launch({ headless: headless, defaultViewport: null });
+    const browser = await puppeteer.launch({ headless: headless, defaultViewport: null, args: ["--start-maximized"] });
     const page = await browser.newPage();
+
     var username;
     var password;
 
@@ -30,7 +32,26 @@ var argv = require("minimist")(process.argv.slice(2));
         process.exit(1);
     }
 
-    /* login */
+    /* setup of test environment */
+    await login(page, username, password);
+    await changeToEditMode(page);
+    await openVisualizationMode(page);
+    await connectToProjectServer(page);
+
+    /* run test suite */
+    await runTestSuites(page);
+
+    /* closedown */
+    if (headless) {
+        // cannot use pdf when not running true headless
+        await page.pdf({ path: "result.pdf", landscape: true });
+
+        // close browser
+        await browser.close();
+    }
+})();
+
+async function login(page, username, password) {
     await page.goto("https://labs.spotfire-cloud.com/spotfire/login.html#/", { waitUntil: "networkidle2" });
     await new Promise((r) => setTimeout(r, 2000)); // wait for loading
     await page.type("[name=username]", username);
@@ -42,7 +63,9 @@ var argv = require("minimist")(process.argv.slice(2));
         { waitUntil: "networkidle2" }
     );
     await new Promise((r) => setTimeout(r, 5000)); // wait for loading
+}
 
+async function changeToEditMode(page) {
     /* change to edit mode */
     await page.waitForSelector(".sfx_simple-dropdown_846");
     await page.click(".sfx_author-dropdown_463");
@@ -51,10 +74,11 @@ var argv = require("minimist")(process.argv.slice(2));
 
     /* remove intro */
     await page.click("#id65");
+}
 
+async function openVisualizationMode(page) {
     /* create visualization mode */
-    await page.click(".sfx_button_462");
-    await page.click(".contextMenuItemLabel[title=Tools]");
+    await page.click(".sfx_menu-item_454[title=Tools]");
     await page.click(".contextMenuItemLabel[title=Development]");
     await page.$$eval(".contextMenuItemLabel", (elements) =>
         elements.forEach((el) => {
@@ -64,7 +88,9 @@ var argv = require("minimist")(process.argv.slice(2));
         })
     );
     await new Promise((r) => setTimeout(r, 5000)); // wait for loading
+}
 
+async function connectToProjectServer(page) {
     /* connect to project */
     await page.click(".sfx_button_507");
     await new Promise((r) => setTimeout(r, 4000)); // wait for loading
@@ -84,14 +110,13 @@ var argv = require("minimist")(process.argv.slice(2));
         })
     );
     await new Promise((r) => setTimeout(r, 4000)); // wait for loading
+    await page.click('[id="id102"');
+}
 
-    /* show results */
-    if (headless) {
-        // cannot use pdf when not running true headless
-        await page.pdf({ path: "result.pdf", landscape: true });
-    }
+async function runTestSuites(page) {
+    const element = await page.$(".sfx_label_217");
+    const text = await page.evaluate((element) => element.textContent, element);
+    console.log(text);
 
-    // continue here with tests
-
-    await browser.close();
-})();
+    console.log("hellloooo");
+}
