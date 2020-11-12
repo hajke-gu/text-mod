adb3b17a6abd5728a0d331bca79d121d6c482152
Synoecism <karl.westgardh@gmail.com>
2020-11-12 20:59:28 +0100

    update minification

--- minification.sh
+++ minification.sh
@@ -5,9 +5,9 @@
 
 sudo npm i html-minifier -g
 sudo npm i minify -g
-ls -man
+ls -man src/
 mkdir src/full
-ls -man
+ls -man src/
 mv src/index.html src/full/index.html
 mv src/main.js src/full/main.js
 mv src/main.css src/full/main.css
